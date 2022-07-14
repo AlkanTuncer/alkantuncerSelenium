0ecee9943930b822679bcb5cff3d073e2fe1400a
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-14 18:31:01 +0300

    Selenium

--- src/test/java/day02_1307_DriverMetotlari/Driver_Metotlar_ImlicitlyWait.java
+++ src/test/java/day02_1307_DriverMetotlari/Driver_Metotlar_ImlicitlyWait.java
@@ -16,8 +16,6 @@ public class Driver_Metotlar_ImlicitlyWait {
         driver.manage().window().maximize();                                  // 16. ve 17. satırlar bizim kalıpla beraber her zaman yazıcaz.
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-
-
         driver.get("https://www.futbin.com");
         driver.close();
 
