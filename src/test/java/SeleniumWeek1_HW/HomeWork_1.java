b7cadb46eed98fa3d588895bc12d24f1f17ca5ec
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-17 11:50:47 +0300

    Selenium Week01

--- src/test/java/SeleniumWeek1_HW/HomeWork_1.java
+++ src/test/java/SeleniumWeek1_HW/HomeWork_1.java
@@ -1,5 +1,13 @@
 package SeleniumWeek1_HW;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class HomeWork_1 {
     public static void main(String[] args) {
 
@@ -11,6 +19,25 @@ public class HomeWork_1 {
         //6.  "kayit yapilamadi" yazdiriniz
         //8.  eger yazi gorunur degilse "kayit yapildi" yazdiriniz
         //9.  Tum sayfalari kapatiniz
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.navigate().to("https://id.heroku.com/login");
+
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("lazkopat53@hotmail.com");
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("12345AT.");
+        driver.findElement(By.xpath("//*[@id=\"login\"]/form/button")).submit();
+
+        if (driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[1]")).getText().equals("There was a problem with your login.")){
+            System.out.println("Kayıt yapılamadı.");
+        }else{
+            System.out.println("Kayıt yapıldı.");
+        }
+
+        driver.close();
 
     }
 }
--- src/test/java/SeleniumWeek1_HW/HomeWork_2.java
+++ src/test/java/SeleniumWeek1_HW/HomeWork_2.java
@@ -1,5 +1,11 @@
 package SeleniumWeek1_HW;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class HomeWork_2 {
     public static void main(String[] args) {
 
@@ -13,6 +19,30 @@ public class HomeWork_2 {
             6. Tekrar “facebook” sayfasina donun
         */
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.navigate().to("https://www.facebook.com");
+
+        if (driver.getTitle().equals("facebook")){
+            System.out.println("Title : facebook");
+        }else{
+            System.out.println("driver.getTitle() = " + driver.getTitle());
+        }
+
+        if (driver.getCurrentUrl().contains("facebook")){
+            System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+        }else{
+            System.out.println("Actual URL : "+driver.getCurrentUrl());
+        }
+
+        driver.navigate().to("https://www.walmart.com");
+        if (driver.getTitle().contains("Walmart.com"))System.out.println("Title 'Walmart.com' içeriyor");
+
+        driver.navigate().back();
+        driver.close();
 
     }
 }
