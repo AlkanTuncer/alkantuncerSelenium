94b50d8ea32499a0bdfb786ff504599ce9ac14ed
AlkanTuncer <alkantuncer53@hotmail.com>
2022-07-18 21:25:18 +0300

    Xpath - CssSelector

---
+++ src/test/java/day04_XPath_CssSelector/CssSelector.java
@@ -0,0 +1,54 @@
+package day04_XPath_CssSelector;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class CssSelector {
+
+    /*
+        css = tagName[attribute name='value'];
+
+        css = "tagName#idValue"     -> Bu ikisi sadece id value ile çalışır.
+        css = "#idValue"
+
+        css = "tagName.classValue"  -> Bu ikisi sadece class value ile çalışır.
+        css = ".classValue"
+     */
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //Verilen web sayfasına gidin. http://a.testaddressbook.com/sign_in
+        driver.navigate().to("http://a.testaddressbook.com/sign_in");
+
+        //Locate email textbox, password textbox, signin button
+        WebElement emailTextBox = driver.findElement(By.cssSelector("input#session_email"));
+        WebElement passwordTB = driver.findElement(By.cssSelector("input[placeholder='Password']"));
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value=\"Sign in\"]"));
+
+        //Asagidaki kullanıcı adını ve şifreyi girin ve sign in duğmesini tıklayın
+        //Username :testtechproed@gmail.com
+        // Password : Test1234!
+        emailTextBox.sendKeys("testtechproed@gmail.com");
+        passwordTB.sendKeys("Test1234!");
+        signInButton.click();
+
+        //NOT: cssSelector kullanarak elementleri locate ediniz.
+
+        WebElement simpleYazisi = driver.findElement(By.tagName("h4"));
+        System.out.println("Sayfadaki Yazı = " + simpleYazisi.getText());
+
+        driver.close();
+
+
+    }
+}
--- src/test/java/day04_XPath_CssSelector/Xpath.java
+++ src/test/java/day04_XPath_CssSelector/Xpath.java
@@ -1,7 +1,14 @@
 package day04_XPath_CssSelector;
 
-public class Xpath {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+public class Xpath {
     // Absolut XPath - Parent/Child ilişkisi
     // Relative XPath - tag name/attribute/value -> Bu 3 web element ile unique sonuc veren xpath yazılır.
     // Relative XPath - //tagName[@attributeAdı="attributeValue"] -> yazımı bu şekildedir.
@@ -17,4 +24,42 @@ public class Xpath {
         By.xpath("//div[@id='logo' or class='flex-col logo']")
         By.xpath("//div[@id='logo' and class='flex-col logo']")
      */
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        // https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        driver.navigate().to("https://the-internet.herokuapp.com/add_remove_elements/");
+
+        // Add Element butonuna 3 kere basin
+        WebElement addButton = driver.findElement(By.xpath("//*[.=\"Add Element\"]"));
+        addButton.click();
+        addButton.click();
+        addButton.click();
+
+        // 2.Delete butonu’nun gorunur oldugunu test edin
+        WebElement deleteButton = driver.findElement(By.xpath("(//*[.=\"Delete\"])[2]"));
+        boolean isDisplayed = deleteButton.isDisplayed();
+        if (isDisplayed){
+            System.out.println("Test PASSED - Delete Button görünüyor.");
+        }else{
+            System.out.println("Test FAILED - Delete Button görünmüyor.");
+        }
+
+        // 3.Delete tusuna basin
+        driver.findElement(By.xpath("(//*[.=\"Delete\"])[3]")).click();
+
+        // *“Add/Remove Elements” yazisinin gorunur oldugunu test edin
+        if (driver.findElement(By.xpath("//*[contains(text(),'Remove Elements')]")).isDisplayed()){
+            System.out.println("Test PASSED - 'Add/Remove Elements' yazısı görünüyor.");
+        }else{
+            System.out.println("Test FAILED");
+        }
+
+        driver.close();
+
+    }
 }
